8ac992ea7ede319916f4c9ca46bb9191a1ed4d63
Fatema-Nawrin <nawrin4000@gmail.com>
2022-05-08 01:17:50 +0600

    header added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,37 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '../../../firebase.init';
+import './Header.css'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <div>
+            <Navbar collapseOnSelect expand="lg" variant="light">
+                <Container>
+                    <Navbar.Brand href="/">Wood Decor Warehouse</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link style={{ color: 'black' }} href="/home">Home</Nav.Link>
+                            <Nav.Link style={{ color: 'black' }} href="/blog">Blogs</Nav.Link>
+
+                            {
+                                user ?
+                                    <Nav.Link style={{ color: 'black' }} href="/login">Login</Nav.Link>
+                                    :
+                                    <Nav.Link style={{ color: 'black' }} href="/login" onClick={handleSignOut}>Signout</Nav.Link>
+                            }
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
         </div>
     );
