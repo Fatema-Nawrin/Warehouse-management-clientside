529dac339bd7018c4d345a9458f39fb8ab29aa1b
Fatema-Nawrin <nawrin4000@gmail.com>
2022-05-07 23:56:02 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './Pages/Blog/Blog';
+import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import Signin from './Pages/Login/Signin/Signin';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Firebase deployment</h1>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signin' element={<Signin></Signin>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
